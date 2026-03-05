98376d870f6dfde53028c62bb3c7c5ac8e750f07
DICZDC <zaito.zd@gmail.com>
2026-03-05 12:26:17 -0600

    Add models, factories, migrations, and seeders for Categoria, Equipo, Solicitud, and Unidad_Equipo

---
+++ app/Models/Solicitud_Equipo.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Solicitud_Equipo extends Model
+{
+    /** @use HasFactory<\Database\Factories\SolicitudEquipoFactory> */
+    use HasFactory;
+}
