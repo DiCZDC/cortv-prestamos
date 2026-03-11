4e8c487f69b769224f0a52d66e193fcc729fb424
Danielinh0 <168349271+Danielinh0@users.noreply.github.com>
2026-03-11 15:05:56 -0600

    Cards ahora si final FINALISIMO
    GRACIAS A DIOS

--- resources/views/components/⚡card.blade.php
+++ resources/views/components/⚡card.blade.php
@@ -7,21 +7,22 @@ new class extends Component
     public $titulo;
     public $icono; 
     public $descripcion;
-    public $bg;
+    public $color_text = 'hueso';
+    public $color_bg;
 
 
-    public function mount($titulo = null, $icono = null, $descripcion=null, $bg=null)
+    public function mount($titulo = null, $icono = null, $descripcion=null, $color_bg = null)   
     {
         $this->titulo = $titulo;
         $this->icono = $icono;
         $this->descripcion = $descripcion;
-        $this->bg = $bg;
+        $this->color_bg = $color_bg;        
     }
 };
 ?>
 
-<div class="w-[254px] h-[180px] bg-[#AE2B2F] rounded-3xl shadow-lg flex flex-col items-center justify-center gap-2 p-5">
-    <flux:icon name="{{ $icono }}" class="w-10 h-10" />
-    <span class="text-hueso font-semibold text-[22px] text-center tracking-[-0.38px] leading-none"> {{ $titulo }} </span>                      
-    <span class="text-hueso font-semibold text-base text-center"> {{ $descripcion }} </span>
+<div class="w-[254px] h-[180px] {{$color_bg}} rounded-3xl shadow-lg flex flex-col items-center justify-center gap-2 p-5">
+    <flux:icon name="{{ $icono }}" class="w-10 h-10 text-{{$color_text}}" />
+    <span class="text-{{$color_text}} font-semibold text-[22px] text-center tracking-[-0.38px] leading-none"> {{ $titulo }} </span>                      
+    <span class="text-{{$color_text}} font-semibold text-base text-center"> {{ $descripcion }} </span>
 </div>
