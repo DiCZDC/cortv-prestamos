7c3715f29ad3068ff312c6fd15130bcece46a826
DICZDC <zaito.zd@gmail.com>
2026-03-13 12:57:38 -0600

    Refactor card data retrieval to use dynamic data for debt and maintenance statistics

--- resources/views/components/⚡4cards/4cards.php
+++ resources/views/components/⚡4cards/4cards.php
@@ -3,6 +3,7 @@
 use Livewire\Component;
 use Livewire\Attributes\Computed;
 use App\Models\{
+    Solicitud,
     Solicitud_Equipo,
     Unidad_Equipo,
     Equipo
@@ -10,21 +11,32 @@ use App\Models\{
 
 new class extends Component
 {
+    #[Computed()]
+    public function cant_mantenimiento(){
+        return Unidad_Equipo::where('estado','=','En reparación')->count();
+    }
+    #[Computed()]
+    public function mas_deudas(){
+        return Solicitud::whereNotNull('fecha_entrega')->
+                    whereColumn('fecha_devolucion', '<', 'fecha_entrega')->
+                    join('users','users.id', '=','solicituds.id_trabajador')->
+                    select('users.name', \DB::raw('count(*) as total'))->
+                    groupBy('users.name')->orderByDesc('total')->limit(5)->
+                    get();
+    }
 
     #[Computed()]
     public function mas_solicitado(){
-        // return DB::table('solicitud__equipos')->join('unidad__equipos','solicitud__equipos.id_unidad_Equipo', '=','unidad__equipos.id')->join('equipos', 'unidad__equipos.id_equipo','=','equipos.id')->get();
-        // return Solicitud_Equipo::join('unidad__equipos','solicitud__equipos.id_unidad_Equipo', '=','unidad__equipos.id')->join('equipos', 'unidad__equipos.id_equipo','=','equipos.id')->groupBy('equipos.modelo')->count();
         return Solicitud_Equipo::join('unidad__equipos','solicitud__equipos.id_unidad_Equipo', '=','unidad__equipos.id')->
                                 join('equipos', 'unidad__equipos.id_equipo','=','equipos.id')->
                                 select('equipos.modelo','equipos.marca', \DB::raw('count(*) as total'))->
-                                groupBy('equipos.modelo','equipos.marca')->orderByDesc('total')->first();
+                                groupBy('equipos.modelo','equipos.marca')->orderByDesc('total')->limit(5);
     }
     #[Computed()]
     public function menos_solicitado(){
         return Solicitud_Equipo::join('unidad__equipos','solicitud__equipos.id_unidad_Equipo', '=','unidad__equipos.id')->
                                 join('equipos', 'unidad__equipos.id_equipo','=','equipos.id')->
                                 select('equipos.modelo','equipos.marca', \DB::raw('count(*) as total'))->
-                                groupBy('equipos.modelo','equipos.marca')->orderBy('total')->first();
+                                groupBy('equipos.modelo','equipos.marca')->orderBy('total')->limit(5);
     }
 };
