ef744cb2f48b8b0d35f2f23ae7446fb1d8c2ecd1
DICZDC <zaito.zd@gmail.com>
2026-03-13 11:16:44 -0600

    Update card descriptions to use dynamic data for most and least requested equipment

--- resources/views/components/⚡4cards/4cards.blade.php
+++ resources/views/components/⚡4cards/4cards.blade.php
@@ -2,7 +2,6 @@
     
     <livewire:card titulo='Mas deudas acumuladas' descripcion='Israel Juarez' icono='thumbs-down' color_bg='bg-rojo_claro' color_text='text-hueso'/>
     <livewire:card titulo='Mantenimiento' descripcion='4 equipos reportados necesitan revision' icono='wrench' color_bg='bg-rojo_oscuro' color_text='text-hueso'/>
-    <livewire:card titulo='Equipo más solicitado' descripcion='Conversor de audio' icono='award' color_text='text-black'/>    
-    <livewire:card titulo='Equipo menos solicitado' descripcion='Conversor de video' icono='trending-down' color_bg='bg-black' color_text='text-hueso'/>
-
+    <livewire:card titulo='Equipo más solicitado' descripcion="{{ $this->mas_solicitado->marca.' '.$this->mas_solicitado->modelo }}" icono='award' color_text='text-black'/>    
+    <livewire:card titulo='Equipo menos solicitado' descripcion="{{ $this->menos_solicitado->marca.' '. $this->menos_solicitado->modelo }}" icono='trending-down' color_bg='bg-black' color_text='text-hueso'/>
 </div>
--- resources/views/components/⚡4cards/4cards.php
+++ resources/views/components/⚡4cards/4cards.php
@@ -1,10 +1,30 @@
 <?php
 
 use Livewire\Component;
-
-
+use Livewire\Attributes\Computed;
+use App\Models\{
+    Solicitud_Equipo,
+    Unidad_Equipo,
+    Equipo
+};
 
 new class extends Component
 {
-    //
+
+    #[Computed()]
+    public function mas_solicitado(){
+        // return DB::table('solicitud__equipos')->join('unidad__equipos','solicitud__equipos.id_unidad_Equipo', '=','unidad__equipos.id')->join('equipos', 'unidad__equipos.id_equipo','=','equipos.id')->get();
+        // return Solicitud_Equipo::join('unidad__equipos','solicitud__equipos.id_unidad_Equipo', '=','unidad__equipos.id')->join('equipos', 'unidad__equipos.id_equipo','=','equipos.id')->groupBy('equipos.modelo')->count();
+        return Solicitud_Equipo::join('unidad__equipos','solicitud__equipos.id_unidad_Equipo', '=','unidad__equipos.id')->
+                                join('equipos', 'unidad__equipos.id_equipo','=','equipos.id')->
+                                select('equipos.modelo','equipos.marca', \DB::raw('count(*) as total'))->
+                                groupBy('equipos.modelo','equipos.marca')->orderByDesc('total')->first();
+    }
+    #[Computed()]
+    public function menos_solicitado(){
+        return Solicitud_Equipo::join('unidad__equipos','solicitud__equipos.id_unidad_Equipo', '=','unidad__equipos.id')->
+                                join('equipos', 'unidad__equipos.id_equipo','=','equipos.id')->
+                                select('equipos.modelo','equipos.marca', \DB::raw('count(*) as total'))->
+                                groupBy('equipos.modelo','equipos.marca')->orderBy('total')->first();
+    }
 };
