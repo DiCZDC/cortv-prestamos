24f41f777318497b1552cb07995dfcd33158f336
DICZDC <zaito.zd@gmail.com>
2026-03-13 14:42:11 -0600

    Remove 'Estado' column and its associated badge logic from the equipos table for simplification

--- resources/views/components/equipo/⚡table/table.blade.php
+++ resources/views/components/equipo/⚡table/table.blade.php
@@ -7,7 +7,6 @@
             <flux:table.column sortable :sorted="$this->sortBy === 'marca'" :direction="$this->sortDirection" wire:click="sort('marca')">Marca</flux:table.column>
             <flux:table.column sortable :sorted="$this->sortBy === 'modelo'" :direction="$this->sortDirection" wire:click="sort('modelo')">Modelo</flux:table.column>
             <flux:table.column>Unidades totales</flux:table.column>
-            <flux:table.column>Estado</flux:table.column>
             <flux:table.column>Acciones</flux:table.column>
         </flux:table.columns>
         @forelse ($this->equipos as $equipo )
@@ -16,17 +15,6 @@
                 <flux:table.cell>{{ $equipo->marca }}</flux:table.cell>
                 <flux:table.cell>{{ $equipo->modelo}}</flux:table.cell>
                 <flux:table.cell>{{ $this->cant_equipos($equipo->id) }}</flux:table.cell>
-                <flux:table.cell>
-                    @if ($this->cant_equipos($equipo->id) > 0)
-                        <flux:badge size="sm" color="green" inset="top bottom">
-                            Disponible
-                        </flux:badge>
-                    @else
-                        <flux:badge size="sm" color="red" inset="top bottom">
-                            No hay unidades disponibles
-                        </flux:badge>
-                    @endif
-                </flux:table.cell>
                 <flux:table.cell>
                     <flux:button variant="ghost" size="sm" icon="ellipsis-horizontal" inset="top bottom"></flux:button>
                 </flux:table.cell>
