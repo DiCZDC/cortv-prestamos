8cd14d1bc43d9716c88695ec837832ca20c093d6
DICZDC <zaito.zd@gmail.com>
2026-03-13 14:13:13 -0600

    Refactor card component layout for improved readability and maintainability

--- resources/views/components/⚡4cards/4cards.blade.php
+++ resources/views/components/⚡4cards/4cards.blade.php
@@ -1,7 +1,29 @@
 <div class="grid grid-cols-2 grid-rows-2 place-items-center content-center">
     
-    <livewire:card titulo='Mas deudas acumuladas' descripcion="{{ $this->mas_deudas->first()?->name }}" icono='thumbs-down' color_bg='bg-rojo_claro' color_text='text-hueso'/>
-    <livewire:card titulo='Mantenimiento' descripcion='{{ $this->cant_mantenimiento ?? 0 }} equipos reportados necesitan revision' icono='wrench' color_bg='bg-rojo_oscuro' color_text='text-hueso'/>
-    <livewire:card titulo='Equipo más solicitado' descripcion="{{ $this->mas_solicitado->first()?->marca.' '.$this->mas_solicitado->first()?->modelo }}" icono='award' color_text='text-black'/>    
-    <livewire:card titulo='Equipo menos solicitado' descripcion="{{ $this->menos_solicitado->first()?->marca.' '. $this->menos_solicitado->first()?->modelo }}" icono='trending-down' color_bg='bg-black' color_text='text-hueso'/>
+    <livewire:card 
+        titulo='Mas deudas acumuladas' 
+        descripcion="{{ $this->mas_deudas->first()?->name }}" 
+        icono='thumbs-down' 
+        color_bg='bg-rojo_claro' 
+        color_text='text-hueso'
+    />
+    <livewire:card 
+        titulo='Mantenimiento' 
+        descripcion='{{ $this->cant_mantenimiento ?? 0 }} equipos reportados necesitan revision' 
+        icono='wrench' 
+        color_bg='bg-rojo_oscuro' color_text='text-hueso'
+    />
+    <livewire:card 
+        titulo='Equipo más solicitado' 
+        descripcion="{{ $this->mas_solicitado->first()?->marca.' '.$this->mas_solicitado->first()?->modelo }}" 
+        icono='award' 
+        color_text='text-black'
+    />    
+    <livewire:card 
+        titulo='Equipo menos solicitado' 
+        descripcion="{{ $this->menos_solicitado->first()?->marca.' '. $this->menos_solicitado->first()?->modelo }}" 
+        icono='trending-down' 
+        color_bg='bg-black' 
+        color_text='text-hueso'
+    />
 </div>
