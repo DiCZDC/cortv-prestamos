8ee77a8fa2abe86b9a06989c32a033cce6c7a3b2
Danielinh0 <168349271+Danielinh0@users.noreply.github.com>
2026-03-13 10:40:54 -0600

    penultimos cambios a las cards

---
+++ resources/views/components/⚡4cards/4cards.blade.php
@@ -0,0 +1,8 @@
+<div class="grid grid-cols-2 grid-rows-2 place-items-center content-center">
+    
+    <livewire:card titulo='Mas deudas acumuladas' descripcion='Israel Juarez' icono='thumbs-down' color_bg='bg-rojo_claro' color_text='text-hueso'/>
+    <livewire:card titulo='Mantenimiento' descripcion='4 equipos reportados necesitan revision' icono='wrench' color_bg='bg-rojo_oscuro' color_text='text-hueso'/>
+    <livewire:card titulo='Equipo más solicitado' descripcion='Conversor de audio' icono='award' color_text='text-black'/>    
+    <livewire:card titulo='Equipo menos solicitado' descripcion='Conversor de video' icono='trending-down' color_bg='bg-black' color_text='text-hueso'/>
+
+</div>
