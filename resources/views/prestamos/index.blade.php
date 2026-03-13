97037bf5620ca9734537ac4e99753a2f564a5cb7
DICZDC <zaito.zd@gmail.com>
2026-03-13 14:48:15 -0600

    Refactor prestamos index layout for improved structure and readability; add box icon component for consistent styling

--- resources/views/prestamos/index.blade.php
+++ resources/views/prestamos/index.blade.php
@@ -1,17 +1,22 @@
 <x-layouts::app :title="__('Prestamos')">
-    <div class=" pt-2 w-3/4 gap-6">
-        {{-- Cabecera principal --}}
-        <div class="mb-4 flex items-center gap-6 text-rojo_claro">
-            <flux:icon name="file" class="inline h-15 w-15" />
-            <span class="ml-2 text-5xl font-semibold">
-                {{ __('Prestamos Activos/Pendientes') }}
-            </span>
+   <div class="flex flex-row gap mb-10">
+        <div class=" pt-2 w-3/4 gap-6">
+            {{-- Cabecera principal --}}
+            <div class="mb-4 flex items-center gap-6 text-rojo_claro">
+                <flux:icon name="file" class="inline h-15 w-15" />
+                <span class="ml-2 text-5xl font-semibold">
+                    {{ __('Prestamos Activos/Pendientes') }}
+                </span>
+            </div>
+            <div class="mb-2 flex items-center text-gris_claro align-middle gap-7">
+                <flux:icon name="database" class="inline h-10 w-10" />
+                <span class="text-[30px] -tracking-tighter text-gris_claro font-inter" style="font-style: normal;">
+                    {{ __('Solicitud de prestamos') }}
+                </span>
+            </div>
         </div>
-        <div class="mb-2 flex items-center text-gris_claro align-middle gap-7">
-            <flux:icon name="database" class="inline h-10 w-10" />
-            <span class="text-[30px] -tracking-tighter text-gris_claro font-inter" style="font-style: normal;">
-                {{ __('Solicitud de prestamos') }}
-            </span>
+        <div class="w-1/4 flex justify-center items-center">
+            <livewire:card titulo='4 Prestamos' descripcion='Pendientes de entrega' icono='box' color_text='black'/>    
         </div>
     </div>
     @livewire('prestamos.table')
