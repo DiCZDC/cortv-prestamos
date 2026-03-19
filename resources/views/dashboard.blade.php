305ef56f617d76ff16d2e103eeef3254ef51114b
DICZDC <zaito.zd@gmail.com>
2026-03-19 10:57:39 -0600

    Refactor dashboard layout by separating admin and trabajador components into their own files

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,30 +1,9 @@
 <x-layouts::app :title="__('Dashboard')">
     @role('admin')
-        <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
-            <div class="grid grid-cols-2 gap-4">
-            
-                @livewire('4cards')
-                
-                <div class="h-full relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-                    <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
-                </div>
-            </div>
-        
-            <div class="grid auto-rows-min gap-4 md:grid-cols-2">
+        @include('dashboard.admin')
 
-                <div class="h-full relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-                    <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
-                </div>
-                <div class="h-full relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-                    <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
-                </div>
-            
-            </div>
-        </div>
     @endrole
     @role('trabajador')
-        <div class="h-full relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-            SOY TRABAJADOR
-        </div>
+        @include('dashboard.trabajador')
     @endrole
 </x-layouts::app>
---
+++ resources/views/dashboard/trabajador.blade.php
@@ -0,0 +1,3 @@
+<div class="h-full relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
+    SOY TRABAJADOR
+</div>
