97b220d98f0832b68581927bc0c297ddc5a98a6f
DICZDC <zaito.zd@gmail.com>
2026-03-19 10:54:01 -0600

    Add 403 error page with app logo and navigation button

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,4 +1,4 @@
 
-<a href="{{route('dashboard')}}">
-    <img src="{{ asset('img/logo_cortv.png') }}" alt="CORTV logo">
+<a href="{{route('dashboard')}}" class="flex items-center gap-2">
+    <img src="{{ asset('img/logo_cortv.png') }}" alt="CORTV logo" class="w-full h-auto">
 </a>
