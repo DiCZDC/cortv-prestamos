8a910f7116d9650ce6d8d09e48e4a5b122c7651f
Danielinh0 <168349271+Danielinh0@users.noreply.github.com>
2026-03-27 14:13:43 -0600

    Garcias por todo zaid
    Pero vamos cerrando el papoi

--- app/View/Components/Componentes/BadgeTable.php
+++ app/View/Components/Componentes/BadgeTable.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace App\View\Components\Componentes;
+namespace App\View\Components;
 
 use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class BadgeTable extends Component
+class badgeTable extends Component
 {
     /**
      * Create a new component instance.
--- app/View/Components/Componentes/BotonHref.php
+++ app/View/Components/Componentes/BotonHref.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class BotonHref extends Component
+class botonHref extends Component
 {
     /**
      * Create a new component instance.
