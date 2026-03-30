0bb32447e17fe4d5d4da2c9c2a7d966f8162a78e
DICZDC <zaito.zd@gmail.com>
2026-03-30 15:17:55 -0600

    Refactor layout in index views for improved responsiveness and 0 gaconsistency

--- resources/views/recepcion/index.blade.php
+++ resources/views/recepcion/index.blade.php
@@ -4,20 +4,23 @@
 
 @endphp
 <x-layouts::app :title="__('Recepción')">
-    <div class="flex flex-col-reverse gap justify-between gap-8.5
-        md:flex-row 
+    <div class="flex flex-col gap justify-between gap-8.5
+        align-middle items-center
+        lg:flex-row 
     "> 
         
         
         {{-- Cabecera principal --}}
-        <div class="flex flex-col justify-between pt-2 w-auto gap-8.5 pl-3 ">
+        <div class="flex flex-col pt-2 w-auto gap-4 pl-3    
+        ">
             <x-componentes.titulo icono="truck" texto="Recepción de Prestamos" />
             <x-componentes.subtitulo icono="database" texto="Devoluciones de productos" />
         </div>
 
         
-        <div class="w-full flex-col flex justify-between items-center gap-8.5
-            lg:w-[40%] lg:flex-row 
+        <div class="w-full flex-col flex justify-between items-center gap-8.5   
+            md:justify-evenly
+            lg:w-[40%] md:flex-row 
         ">
             <livewire:card titulo='{{$cant_esperados}} equipos' descripcion='esperados hoy' icono='inbox' color_text='black'/>    
             <livewire:card titulo='{{$cant_atrasados}} Prestamos' descripcion='atrasados' icono='clock' color_text='black'/>    
