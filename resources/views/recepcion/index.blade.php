54ca9c881522a4eb9a6679b4752963ad1f647ceb
DICZDC <zaito.zd@gmail.com>
2026-03-30 15:03:04 -0600

    Refactor layout structure in index views for improved responsiveness and consistency

--- resources/views/recepcion/index.blade.php
+++ resources/views/recepcion/index.blade.php
@@ -4,15 +4,21 @@
 
 @endphp
 <x-layouts::app :title="__('Recepción')">
-    <div class="flex flex-row gap"> 
-        <div class=" pt-2 w-1/2 gap-6">
-            {{-- Cabecera principal --}}
-            <div class="flex flex-col justify-center gap-8.5 pl-3 ">
-                <x-componentes.titulo icono="truck" texto="Recepción de Prestamos" />
-                <x-componentes.subtitulo icono="database" texto="Devoluciones de productos" />
-            </div>
+    <div class="flex flex-col-reverse gap justify-between gap-8.5
+        md:flex-row 
+    "> 
+        
+        
+        {{-- Cabecera principal --}}
+        <div class="flex flex-col justify-between pt-2 w-auto gap-8.5 pl-3 ">
+            <x-componentes.titulo icono="truck" texto="Recepción de Prestamos" />
+            <x-componentes.subtitulo icono="database" texto="Devoluciones de productos" />
         </div>
-        <div class="w-2/5 flex justify-between items-center ">
+
+        
+        <div class="w-full flex-col flex justify-between items-center gap-8.5
+            lg:w-[40%] lg:flex-row 
+        ">
             <livewire:card titulo='{{$cant_esperados}} equipos' descripcion='esperados hoy' icono='inbox' color_text='black'/>    
             <livewire:card titulo='{{$cant_atrasados}} Prestamos' descripcion='atrasados' icono='clock' color_text='black'/>    
         
