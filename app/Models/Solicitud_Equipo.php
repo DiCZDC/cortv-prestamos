47cdf9ef38b0ff79996f632008893fe2d6546255
DICZDC <zaito.zd@gmail.com>
2026-04-06 15:12:56 -0600

    feat: Add Solicitud_Equipo model with relationships and fillable attributes

---
+++ app/Models/Solicitud_Equipo.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\SolicitudEquipoFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Solicitud_Equipo extends Model
+{
+    /** @use HasFactory<SolicitudEquipoFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'equipo_id',
+        'usuario_id',
+        'fecha_solicitud',
+        'fecha_devolucion',
+        'estado',
+    ];
+
+    public function unidad_equipo()
+    {
+        return $this->belongsTo(Unidad_Equipo::class, 'id_unidad_Equipo');
+    }
+
+    public function solicitud()
+    {
+        return $this->belongsTo(Solicitud::class, 'id_solicitud');
+    }
+}
