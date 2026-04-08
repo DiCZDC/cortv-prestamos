64c3de0dbe12f794c19b9ad173e6d0556fd3f995
DICZDC <zaito.zd@gmail.com>
2026-04-08 13:03:32 -0600

    refactor: Simplify date handling in calendar view and update subtitle to reflect current month

--- resources/views/calendario/index.blade.php
+++ resources/views/calendario/index.blade.php
@@ -1,16 +1,5 @@
 @php
     $meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
-    $hoy = now();
-
-    function subMonth()
-    {
-        $this->hoy = $this->hoy->subMonth();
-    }
-
-    function addMonth()
-    {
-        $this->hoy = $this->hoy->addMonth();
-    }
 
 @endphp
 <x-layouts::app :title="__('Calendario')">
@@ -18,22 +7,15 @@
         {{-- Cabecera principal --}}
         <div class="flex flex-col justify-center gap-8.5 pl-3 mb-8 ">
             <x-componentes.titulo icono="calendar-days" texto="Calendario" />
-            <x-componentes.subtitulo class="w-full" icono="box" texto="Prestamos y Recepciones Este Mes" />
+            <x-componentes.subtitulo class="w-full" icono="box" texto="Prestamos y Recepciones en {{ $meses[now()->month - 1] }} {{ now()->year }}" />
         </div>
     </div>
 
     <div class="flex flex-col justify-center items-center">
-        {{-- <div class="flex justify-center items-center gap-5 mb-2 text-rojo_claro">
-            <flux:button icon="chevron-left" wire:click="subMonth"/>
-            <h1 class="text-6xl">
-                {{ $meses[$hoy->month - 1] }} {{ $hoy->year }}
-            </h1>
-            <flux:button icon="chevron-right" wire:click="addMonth"/>
-        </div> --}}
         <div >
             <livewire:calendario_admin 
-                initial-year="{{ $hoy->year }}"
-                initial-month="{{ $hoy->month }}"
+                initial-year="{{ now()->year }}"
+                initial-month="{{ now()->month }}"
             />
         </div>
     </div>
