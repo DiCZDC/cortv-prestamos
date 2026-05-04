a708263f2ddeec30602d8604661deefb45ea059d
DICZDC <zaito.zd@gmail.com>
2026-05-04 11:50:45 -0600

    feat: Add 'Pendiente' filter option and enhance display of loan details in the show view

--- resources/views/components/archivo/show/⚡table/table.php
+++ resources/views/components/archivo/show/⚡table/table.php
@@ -2,12 +2,19 @@
 
 use Livewire\Component;
 use App\Models\Solicitud_Equipo;
+use App\Models\Solicitud;
+use Livewire\Attributes\Computed;
 
 new class extends Component
 {
     public $solicitudId;
 
+    #[Computed()]
+    public function solicitud(){
+        return Solicitud::find($this->solicitudId);
+    }
+    #[Computed()]
     public function detalles(){
         return Solicitud_Equipo::where('id_solicitud', $this->solicitudId)->get();
-    }    
+    }
 };
--- resources/views/vistas/archivo/show.blade.php
+++ resources/views/vistas/archivo/show.blade.php
@@ -8,28 +8,29 @@
         <flux:breadcrumbs.item href="#">{{ $Solicitud->motivo. ' - ' . $Solicitud->fecha_prestamo }}</flux:breadcrumbs.item>
     </flux:breadcrumbs>
 
-    <div class="flex flex-col items-start justify-center gap-4 pt-1 ">               
+    <div class="flex flex-col items-start justify-center gap-4 pt-1 mb-4">               
             <div class="flex flex-col items-start justify-center gap-7 pl-3  ">               
-                <x-componentes.titulo icono="book-marked" texto="Detalles de la recepción" />
-                <x-componentes.subtitulo icono="square-user-round" texto=" {{ __('Solicitud de:') }} {{ $Solicitud->trabajador->name }}" />
+                <x-componentes.titulo icono="book-marked" texto="Detalles del prestamo" />
+                <x-componentes.subtitulo icono="square-user-round" texto=" {{ __('Solicitado por:') }} {{ $Solicitud->trabajador->name }}" />
             </div> 
             <div class="pl-3 mt-2">
-                <x-componentes.subtitulo icono="shield-user" texto=" {{ __('Autorizada por:') }} {{ $Solicitud->admin->name }}" />
+                <x-componentes.subtitulo icono="shield-user" texto=" {{ __('Autorizado por:') }} {{ $Solicitud->admin->name }}" />
             </div>
     </div>
     
-    <div class="bg-white rounded-lg shadow-md px-5 py-7 flex flex-col gap-3 ">
-            
-            <div class="inline-flex items-center text-gris_claro gap-3 ml-5">
-                <flux:icon.clipboard-paste class="size-10" />
-                <span class="font-medium text-3xl">Equipo Solicitado</span>  
-            </div>
-
-            
-            <div class="px-10 h-9/10 w-full ">
-                {{-- <livewire:recepcion.show.table from="{{ $Solicitud->fecha_prestamo }}" to="{{ $Solicitud->fecha_devolucion }}" :solicitudId="$id" lazy/> --}}
-                <livewire:archivo.show.table :solicitudId="$id" lazy/>
-            </div>
+    <div class="w-full flex justify-around">
+        <div class="bg-white rounded-lg shadow-md  flex flex-col gap-3 px-5 py-7 w-1/2">
+                <div class="inline-flex items-center text-gris_claro gap-3 ml-5">
+                    <flux:icon.clipboard-paste class="size-10" />
+                    <span class="font-medium text-3xl">Equipo Solicitado</span>  
+                </div>
+                <div class="px-10 w-full">
+                    <livewire:archivo.show.table :solicitudId="$id" lazy/>
+                </div>
+        </div>
+        <div class=" flex items-center justify-center">
+            <livewire:calendario.small :id="$id" lazy/>
+        </div>
     </div>
 
 
