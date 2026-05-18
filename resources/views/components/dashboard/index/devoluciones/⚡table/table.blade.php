10ed9b02afe46f7e5918bac2e901e78c909b54ad
DICZDC <zaito.zd@gmail.com>
2026-05-18 12:22:18 -0600

    fix: Update table headers and data retrieval for overdue loans

--- resources/views/components/dashboard/index/devoluciones/⚡table/table.blade.php
+++ resources/views/components/dashboard/index/devoluciones/⚡table/table.blade.php
@@ -7,23 +7,23 @@
     <div class="pt-3 px-10 pb-6" >
     
         @placeholder
-            <x-placeholder.table :header="['','Equipo', 'Solicitante', 'Fecha']"/>
+            <x-placeholder.table :header="['','Equipo', 'Solicitante', 'Días Atrasados']"/>
         @endplaceholder
         <flux:table :paginate="$this->atrasados">
             <flux:table.columns>
                 <flux:table.column class="hidden md:block"></flux:table.column>
-                <x-componentes.header_table> Equipo </x-componentes.header_table>
+                <x-componentes.header_table> Motivo </x-componentes.header_table>
                 @role('admin')
                      <x-componentes.header_table> Solicitante </x-componentes.header_table>
                 @endrole
-                <x-componentes.header_table> Fecha </x-componentes.header_table>    
+                <x-componentes.header_table> Días Atrasados </x-componentes.header_table>    
             </flux:table.columns>
             <flux:table.row>
                 @forelse ($this->atrasados as $prestamo)
                 
                 @php
-                    $dias = round(
-                        \Carbon\Carbon::parse($prestamo->solicitud->fecha_devolucion)
+                    $dias = floor(
+                        \Carbon\Carbon::parse($prestamo->fecha_devolucion)
                             ->diffInDays(now())
                     );
                 @endphp
@@ -32,18 +32,19 @@
                     <flux:table.cell class="hidden md:block">
                         <flux:icon name="video" />
                     </flux:table.cell>
-                    <flux:table.cell class="text-balance!">{{ $prestamo->unidad_equipo->equipo->marca . ' ' . $prestamo->unidad_equipo->equipo->modelo }}</flux:table.cell>
+                    <flux:table.cell class="text-balance!">{{ $prestamo->motivo}}</flux:table.cell>
                     
                     @role('admin')
-                        <flux:table.cell class="text-balance!">{{ $prestamo->nombre_trabajador }}</flux:table.cell>
+                        <flux:table.cell class="text-balance!">{{ $prestamo->trabajador->name }}</flux:table.cell>
                     @endrole
                     <flux:table.cell>  
-                        <x-componentes.badgeTable 
-                            :dias="$dias"
-                            :colores="['urgente' => '!bg-naranja_logo', 'proximo' => '!bg-rojo_claro', 'normal' => '!bg-rojo_oscuro']"
+                        {{-- <x-componentes.badgeTable 
+                        :dias="$dias"
+                        :colores="['urgente' => '!bg-naranja_logo', 'proximo' => '!bg-rojo_claro', 'normal' => '!bg-rojo_oscuro']"
                         >
-                            {{ $dias }} dias
-                        </x-componentes.badgeTable>
+                        {{ $dias }} dias
+                    </x-componentes.badgeTable> --}}
+                    <x-componentes.boton-href ruta="recepcion.show" texto="{{ $dias }} dias" icon="eye" :id="$prestamo->id" />    
                     </flux:table.cell>
                 </flux:table.row>
             
--- resources/views/components/dashboard/index/devoluciones/⚡table/table.php
+++ resources/views/components/dashboard/index/devoluciones/⚡table/table.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Models\Solicitud_Equipo;
+use App\Models\Solicitud;
 use App\Models\User;
 use Livewire\Attributes\Computed;
 use Livewire\Component;
@@ -17,19 +17,32 @@ new class extends Component
     {
         $user = $this->id_user ? User::find($this->id_user) : auth()->user();
 
-        return Solicitud_Equipo::query()
-            ->join('solicituds', 'solicitud__equipos.id_solicitud', '=', 'solicituds.id')
-            // Filtrar por fecha de devolución menor a la fecha actual y mayor o igual a 30 días atrás
-            ->where('solicituds.fecha_devolucion', '<', now())
-            ->where('solicituds.fecha_devolucion', '>=', now()->subDays(30))
-            // La solicitud debe estar en estado "Entregada" y no "Devuelta"
-            ->where('solicituds.estado', 'Entregada')
-            ->join('users', 'solicituds.id_trabajador', '=', 'users.id')
-            ->select('solicitud__equipos.*', 'users.name as nombre_trabajador')
-            ->orderBy('solicituds.fecha_devolucion', 'asc')
-            ->when($user->hasRole('trabajador'), function ($query) use ($user) {
+        // return Solicitud_Equipo::query()
+        //     ->join('solicituds', 'solicitud__equipos.id_solicitud', '=', 'solicituds.id')
+        //     // Filtrar por fecha de devolución menor a la fecha actual y mayor o igual a 30 días atrás
+        //     ->where('solicituds.fecha_devolucion', '<', now())
+        //     ->where('solicituds.fecha_devolucion', '>=', now()->subDays(30))
+        //     // La solicitud debe estar en estado "Entregada" y no "Devuelta"
+        //     ->where('solicituds.estado', 'Entregada')
+        //     ->join('users', 'solicituds.id_trabajador', '=', 'users.id')
+        //     ->select('solicitud__equipos.*', 'users.name as nombre_trabajador')
+        //     ->orderBy('solicituds.fecha_devolucion', 'asc')
+        //     ->when($user->hasRole('trabajador'), function ($query) use ($user) {
+        //         $query->where('solicituds.id_trabajador', $user->id);
+        //     })
+        //     ->paginate(4);
+        return Solicitud::query()
+            ->when($user && $user->hasRole('trabajador'), function ($query) use ($user) {
                 $query->where('solicituds.id_trabajador', $user->id);
             })
+            ->where('estado', 'Entregada')
+            ->whereNull('fecha_entrega')
+            ->where('fecha_devolucion', '<', now())
+            ->where('fecha_devolucion', '>=', now()->subDays(30))
+            ->orderBy('fecha_devolucion', 'asc')
+            ->join('users', 'solicituds.id_trabajador', '=', 'users.id')
+            ->select('solicituds.*', 'users.name as nombre_trabajador')
+            
             ->paginate(4);
 
     }
