c525c74b9913114688ec514ecdf4d06edf5c32a7
crstvbl <zaito.zd@gmail.com>
2026-06-02 14:07:55 -0600

    correciones

--- app/View/Components/CardWelcome.php
+++ app/View/Components/CardWelcome.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class card-welcome extends Component
+class CardWelcome extends Component
 {
     /**
      * Create a new component instance.
